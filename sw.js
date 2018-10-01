a062f252b86c8b75b203cd7bdff7020e1c5a5202
bhansa <bsaraswat@deloitte.com>
2018-10-01 20:45:02 +0530

    added sw events and updated manifest, added fb id

--- sw.js
+++ sw.js
@@ -1,12 +1,24 @@
+/*
+  ============================================
+  		Service workers are awesome 
+  ============================================
+*/
+
+
+
+const staticVersion = 0.1;
+
 // cache resources when install complete...
 self.addEventListener('install', function(e){
 	e.waitUntil(
-		caches.open('bhansa-blog').then(function(cache){
+		caches.open('bhansa-blog-' + staticVersion).then(function(cache){
 			return cache.addAll([
 				'index.html',
 				'/assets/images/cover.jpeg',
 				'/assets/images/cover-medium.jpeg',
-				'/assets/images/cover-small.jpeg'
+				'/assets/images/cover-small.jpeg',
+				'/assets/css/main.css',
+				'/assets/css/screen.css'
 			]);
 		})
 	);
@@ -22,3 +34,19 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+
+// activated sw
+self.addEventListener('activate', function (event) {
+  console.log('Activated', event);
+});
+
+// add push event listener 
+self.addEventListener('push', function(event){
+	console.log('push message', event);
+	// show push notifications
+	self.registration.showNotification('Push message', {
+		body: 'hey bhansa',
+		icon: '/assets/images/icons/icon-96x96.png'
+	})
+})
